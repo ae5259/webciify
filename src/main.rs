cd9b5f11160fe063b509d9f5bcdfd6199f7c20e8
Aleksei Rybin <aleksei@tapni.su>
2025-09-14 19:51:27 +0700

    fix everything blya

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
-use axum::{routing::get, Router, Json, extract::Query};
-use tapciify::{image_to_ascii, DEFAULT_ASCII_STRING, DEFAULT_FONT_RATIO};
 mod structs;
-use structs::{AsciiResponse, QueryParams};
 
+use axum::{extract::Query, routing::get, Json, Router};
+use image::imageops::FilterType;
+use structs::{AsciiResponse, QueryParams};
+use tapciify::{
+    AsciiArtConverter, AsciiArtConverterOptions, CustomRatioResize, DEFAULT_FONT_RATIO,
+};
 
 #[tokio::main]
-async fn main(){
-    let app = Router::new()
-        .route("/", get(send_ascii));
-
+async fn main() {
+    let app = Router::new().route("/", get(send_ascii));
 
     axum::Server::bind(&"127.0.0.1:3000".parse().unwrap())
         .serve(app.into_make_service())
@@ -16,20 +17,26 @@ async fn main(){
         .unwrap();
 }
 
-
 async fn send_ascii(query: Query<QueryParams>) -> Json<AsciiResponse> {
-    let img_bytes = reqwest::get(&query.image).await.unwrap().bytes().await.unwrap();
-
+    let img_bytes = reqwest::get(&query.image)
+        .await
+        .unwrap()
+        .bytes()
+        .await
+        .unwrap();
     let image = image::load_from_memory(&img_bytes).unwrap();
-    
-    let ascii_img = image_to_ascii(
-        image,
-        query.width,
-        DEFAULT_ASCII_STRING, 
-        false, 
-        DEFAULT_FONT_RATIO);
-    println!("{}", &query.image);
-    
 
-    Json(AsciiResponse { message: (ascii_img.result) })
+    let ascii_image = image
+        .resize_custom_ratio(
+            Some(query.width),
+            None,
+            DEFAULT_FONT_RATIO,
+            FilterType::Triangle,
+        )
+        .ascii_art(&AsciiArtConverterOptions::default())
+        .unwrap();
+
+    Json(AsciiResponse {
+        message: ascii_image.to_string(),
+    })
 }
